5be3de77b5853cd72b7841806208b7ac6eab4833
Kamezaki-jp <champagne.es335@gmail.com>
2021-06-22 11:47:57 +0900

    練習問題8

---
+++ Java_test/src/day5/Problem5_8.java
@@ -0,0 +1,30 @@
+package day5;
+
+public class Problem5_8 {
+
+	public static void main(String[] args) {
+		int[] a = new int[5];
+		
+		for (int i = 0; i < a.length; i++) {
+			int num = (int)(Math.random() * 20) - 10;
+			a[i] = num;
+			System.out.print(a[i] + " ");
+		}
+		
+		int over = 0;
+		int under = 0;
+		int zero = 0;
+		for (int i = 0; i < a.length; i++) {
+			if ( 0 < a[i]) {
+				over++;
+			}else if (0 > a[i]) {
+				under++;
+			} else if (0 == a[i]) {
+				zero++;
+			}
+		}
+		System.out.print("\n" + "0より大きい数:" + over);
+		System.out.print("\n" + "0より小さい数:" + under);
+		System.out.print("\n" + "0だった数:" + zero);
+	}
+}
